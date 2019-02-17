c20f079b49c8abc9a0c6703e5b52a97a85d3d0b9
Jake North <jakewnorth@gmail.com>
2019-02-16 20:45:46 -0800

    Update base.js

--- server/static/base.js
+++ server/static/base.js
@@ -5,6 +5,9 @@ function count(){
 $( document ).ready(function(){
   count();
   $(".counter").click(count);
+  $('.ui.modal').onApprove(function(){
+  	alert("You approved it!")
+  })
   $("#newfunc").click(function(){
   	$('.ui.modal')
   		.modal('show');
