17ea06adfb1e30cec00ff121ab7bf4fcde8bef4a
Jake North <jakewnorth@gmail.com>
2019-02-17 00:55:39 -0800

    se

--- server/static/base.js
+++ server/static/base.js
@@ -10,10 +10,8 @@ $( document ).ready(function(){
   	funcName = $("#funcIN").val();
   	args = $("#argsIN").val();
   	$.get("/api/nametofunc/"+funcName, function(payload){
-  		alert("got payload");
   		$.get("/api/run/"+payload, function(result){
-  			alert("done!")
-  			alert(result);
+  			$("#out").html(result);
   		});
   	});
 
@@ -24,7 +22,6 @@ $( document ).ready(function(){
       name = $("#nameIN").val();
       func = $("#functionIN").val();
       $.get("/api/func/"+name+"/"+encodeURIComponent(func));
-      alert(encodeURIComponent(func))
     }
   });
 
